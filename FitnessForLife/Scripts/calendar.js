45a36fafab270bc9510400044b501a1b808093c1
Aditi <amuk0002@student.monash.edu>
2018-10-09 16:57:29 +1100

    First commit.

---
+++ FitnessForLife/Scripts/calendar.js
@@ -0,0 +1,23 @@
+﻿var appointments = [];
+$(".appointments").each(function () {
+    var title = $(".title", this).text().trim();
+    var start = $(".start", this).text().trim();
+    var appointment = {
+        "title": title,
+        "start": start
+    };
+    appointments.push(appointment);
+});
+
+$("#calendar").fullCalendar({
+    locale: 'au',
+    events: appointments,
+
+    dayClick: function (date, allDay, jsEvent, view) {
+        var d = new Date(date);
+        var m = moment(d).format("YYYY-MM-DD");
+        m = encodeURIComponent(m);
+        var uri = "/Events/Create?date=" + m;
+        $(location).attr('href', uri);
+    }
+});
---
+++ FitnessForLife/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FitnessForLife.Startup))]
+namespace FitnessForLife
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
