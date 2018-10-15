bf00498ab12b44f1f70a6cc8fb456a546266b277
Aditi <amuk0002@student.monash.edu>
2018-10-15 13:54:37 +1100

    commit

--- FitnessForLife/Controllers/AccountController.cs
+++ FitnessForLife/Controllers/AccountController.cs
@@ -164,7 +164,7 @@ namespace FitnessForLife.Controllers
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("Create", "Users");
                 }
                 AddErrors(result);
             }
