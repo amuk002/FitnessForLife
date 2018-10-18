6524278963b1179fec53ed149e1dfd95f945e307
Aditi <amuk0002@student.monash.edu>
2018-10-19 10:03:50 +1100

    commit

--- FitnessForLife/App_Start/BundleConfig.cs
+++ FitnessForLife/App_Start/BundleConfig.cs
@@ -8,8 +8,12 @@ namespace FitnessForLife
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/lib").Include(
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/bootstrap.js",
+                        "~/Scripts/respond.js",
+                        "~/Scripts/DataTables/jquery.dataTables.js",
+                        "~/Scripts/DataTables/dataTables.bootstrap4.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -19,14 +23,11 @@ namespace FitnessForLife
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
-
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap-mytheme.css",
                       "~/Content/site.css",
-                      "~/Content/fullcalendar.min.css"));
+                      "~/Content/fullcalendar.min.css",
+                      "~/Content/DataTables/css/dataTables.bootstrap4.css"));
 
             bundles.Add(new ScriptBundle("~/bundles/fullcalendar").Include(
                      "~/Scripts/lib/jquery.min.js",
